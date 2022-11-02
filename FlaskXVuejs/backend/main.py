a1e0f77336e5779bec44fcde028367ad3f4e9ea2
LedharniWU <43802881+LedharniWU@users.noreply.github.com>
2022-11-02 17:36:18 +0900

    Add files via upload

--- FlaskXVuejs/backend/main.py
+++ FlaskXVuejs/backend/main.py
@@ -4,17 +4,27 @@ from api import api_bp
 app = Flask(__name__, static_folder='../frontend/dist/static', template_folder='../frontend/dist')
 app.register_blueprint(api_bp)
 
-info = []
+files = []
+idx   = 0
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
     return render_template('index.html')
 
-@app.route('/post_info', methods=['POST'])
-def get_info():
+@app.route('/file', methods=['POST'])
+def create_file():
+    global idx
     post_data = request.get_json()
+    new_file = {'id': idx}
+    new_file.update(list(post_data.items()))
+    files.append(new_file)
+    idx = idx + 1
+    return jsonify({'files': files})
 
+@app.route('/files')
+def get_files():
+    return jsonify({'files': files})
 
 if __name__ == '__main__':
     app.run()
